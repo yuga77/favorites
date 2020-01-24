9939161037619a009c7a855026cc080b2e1d1aaa
yuga77 <y-n7c8@i.softbank.jp>
2020-01-24 13:48:01 +0900

    create contents_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :genres, only: :index do 
-    resoureces :contents, only[:index]
+    resources :contents, only: :index
   end
 end
